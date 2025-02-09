8c6d05ab8563e3e77bfa2ab66595ebc08e1f6d3f
Kanchan <kanchansingh30nov@gmail.com>
2025-02-09 21:26:01 +0530

    Page redirection

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import { Link } from "react-router";
+import { Link,  } from "react-router";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -9,11 +9,11 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">IT Company</h1>
         <div className="hidden md:flex space-x-6">
-          <a href="/" className="hover:underline">Home</a>
-          <a href="/about" className="hover:underline">About</a>
-          <a href="/services" className="hover:underline">Services</a>
-          <a href="/portfolio" className="hover:underline">Portfolio</a>
-          <a href="/contact" className="hover:underline">Contact</a>
+          <Link to="/" className="hover:underline">Home</Link>
+          <Link to="/about" className="hover:underline">About</Link>
+          <Link to="/services" className="hover:underline">Services</Link>
+          <Link to="/portfolio" className="hover:underline">Portfolio</Link>
+          <Link to="/contact" className="hover:underline">Contact</Link>
         </div>
         <button 
           onClick={() => setIsOpen(!isOpen)} 
@@ -24,11 +24,11 @@ const Navbar = () => {
       </div>
       {isOpen && (
         <div className="md:hidden flex flex-col  text-blue-200 p-2 space-y-2">
-          <a href="/" onClick={() => setIsOpen(false)}>Home</a>
-          <a href="/about" onClick={() => setIsOpen(false)}>About</a>
-          <a href="/services" onClick={() => setIsOpen(false)}>Services</a>
-          <a href="/portfolio" onClick={() => setIsOpen(false)}>Portfolio</a>
-          <a href="/contact" onClick={() => setIsOpen(false)}>Contact</a>
+          <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+          <Link to="/services" onClick={() => setIsOpen(false)}>Services</Link>
+          <Link to="/portfolio" onClick={() => setIsOpen(false)}>Portfolio</Link>
+          <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
         </div>
       )}
     </nav>
